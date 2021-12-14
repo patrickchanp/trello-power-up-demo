f1dc2deb0c3bf72c84996dec6345da4dae877235
pengfei <pengfei.qian@thoughtworks.com>
2021-12-15 02:23:41 +0800

    modal

--- src/card-button/capability.ts
+++ src/card-button/capability.ts
@@ -5,10 +5,44 @@ export function getCardButton(_t: Trello.PowerUp.IFrame, props: { baseUrl: strin
     return [{
         icon: props.baseUrl + props.icon.dark,
         text: 'Add a Note',
-        callback: (tc: Trello.PowerUp.IFrame) => tc.popup({
-            title: 'Add a Note',
-            url: './model.html',
-            height: 300
+        callback: (tc: Trello.PowerUp.IFrame) => tc.modal({
+            // the url to load for the iframe
+            url: './modal.html',
+            // optional arguments to be passed to the iframe as query parameters
+            // access later with t.arg('text')
+            args: { text: 'Hello' },
+            // optional color for header chrome
+            accentColor: '#F2D600',
+            // initial height for iframe
+            height: 500, // not used if fullscreen is true
+            // whether the modal should stretch to take up the whole screen
+            fullscreen: true,
+            // optional function to be called if user closes modal (via `X` or escape, etc)
+            callback: () => console.log('Goodbye.'),
+            // optional title for header chrome
+            title: 'appear.in meeting',
+            // optional action buttons for header chrome
+            // max 3, up to 1 on right side
+            actions: [{
+                icon: './images/icon.svg',
+                url: 'https://google.com',
+                alt: 'Leftmost',
+                position: 'left',
+            }, {
+                icon: './images/icon.svg',
+                callback: (tr) => tr.popup({
+                    title: tr.localizeKey('appear_in_settings'),
+                    url: 'settings.html',
+                    height: 164,
+                }),
+                alt: 'Second from left',
+                position: 'left',
+            }, {
+                icon: './images/icon.svg',
+                callback: () => console.log(':tada:'),
+                alt: 'Right side',
+                position: 'right',
+            }],
         })
     }];
 }
