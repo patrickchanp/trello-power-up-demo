707f0a9d92d1d5c0b06bc1651be48e2fc49e40e1
pengfei <pengfei.qian@thoughtworks.com>
2021-12-14 17:59:47 +0800

    card button 23

--- src/card-button/CardButton.tsx
+++ src/card-button/CardButton.tsx
@@ -5,23 +5,23 @@ export function getCardButton(_t: Trello.PowerUp.IFrame, props: CapabilityProps)
     return [{
         icon: 'https://cdn-icons-png.flaticon.com/128/1054/1054150.png',
         text: 'trello-power-up-demo',
-        // callback: (tc: Trello.PowerUp.IFrame) => tc.popup({
-        //     title: 'trello-power-up-demo',
-        //     url: './card-button.html',
-        //     height: 300
-        // })
-        callback: (tc: Trello.PowerUp.IFrame) => tc.modal({
+        callback: (tc: Trello.PowerUp.IFrame) => tc.popup({
             title: 'trello-power-up-demo',
-            accentColor: undefined,
-            actions: [],
-            args: {},
-            fullscreen: true,
-            // height: ,
-            url: "",
-            callback(): any[] {
-                return [];
-            }
-
+            url: './card-button.html',
+            height: 300
         })
+        // callback: (tc: Trello.PowerUp.IFrame) => tc.modal({
+        //     title: 'trello-power-up-demo',
+        //     accentColor: undefined,
+        //     actions: [],
+        //     args: {},
+        //     fullscreen: true,
+        //     // height: ,
+        //     url: "",
+        //     callback(): any[] {
+        //         return [];
+        //     }
+        //
+        // })
     }];
 }
