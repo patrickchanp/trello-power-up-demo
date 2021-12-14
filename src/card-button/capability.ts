de8f1c1dedcd0f52325352ef619ba61ecb09240b
pengfei <pengfei.qian@thoughtworks.com>
2021-12-15 02:09:28 +0800

    init again

--- src/card-button/capability.ts
+++ src/card-button/capability.ts
@@ -1,7 +1,7 @@
-import {Trello} from "../../types/trello";
-import {CapabilityProps} from "../../types/power-up";
+import {Trello} from "../types/trello";
+import {CapabilityProps} from "../types/power-up";
 
-export function getCardButton(_t: Trello.PowerUp.IFrame, props: CapabilityProps): Trello.PowerUp.CardButton[] {
+export function getCardButton(_t: Trello.PowerUp.IFrame, props: { baseUrl: string, icon: { light: string, dark: string } }): Trello.PowerUp.CardButton[] {
     return [{
         icon: props.baseUrl + props.icon.dark,
         text: 'Add a Note',
