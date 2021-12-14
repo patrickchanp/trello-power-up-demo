cb45a4682c53187e02370b81192e8b24d1322bce
pengfei <pengfei.qian@thoughtworks.com>
2021-12-14 16:34:47 +0800

    card button 13

--- src/card-button/CardButton.tsx
+++ src/card-button/CardButton.tsx
@@ -12,7 +12,7 @@ export function getCardButton(_t: Trello.PowerUp.IFrame, props: CapabilityProps)
         // })
         callback: (tc: Trello.PowerUp.IFrame) => tc.modal({
             // the url to load for the iframe
-            url: 'http://google.com',
+            url: 'https://google.com',
             // optional arguments to be passed to the iframe as query parameters
             // access later with t.arg('text')
             args: { text: 'Hello' },
